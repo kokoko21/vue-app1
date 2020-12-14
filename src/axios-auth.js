439216a52c4c60889f4e83d2602c1bc5fde7e9db
kokoko21 <72850097+kokoko21@users.noreply.github.com>
2020-12-14 12:15:08 +0900

    Update axios-auth.js

--- src/axios-auth.js
+++ src/axios-auth.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: "https://firestore.googleapis.com/v1/projects/vuejs-axios-15c6b/databases/(default)/documents"
+  baseURL: "https://firestore.googleapis.com/v1/projects/vue************/databases/(default)/documents"
 
 });
 
