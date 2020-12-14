d11b2f33ab9c2af7835430ce1a18c6df16eb2aab
kokoko21 <72850097+kokoko21@users.noreply.github.com>
2020-12-14 12:14:43 +0900

    Update SubmitHome.vue

--- src/views/SubmitHome.vue
+++ src/views/SubmitHome.vue
@@ -42,7 +42,7 @@ export default {
     createComment() {
       // データをサーバに送る（post）
       axios.post( // url, 送るデータ
-        'https://firestore.googleapis.com/v1/projects/vuejs-axios-15c6b/databases/(default)/documents/comments',
+        'https://firestore.googleapis.com/v1/projects/vue************/databases/(default)/documents/comments',
         {
           fields: {
             name: {
